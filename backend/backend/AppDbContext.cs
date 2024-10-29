935d9431148b664718b81d10c7b1535e10cda4f2
Anja Lovric <anja.lovric@gmail.com>
2024-10-29 17:45:12 +0100

    feat: db connection

---
+++ backend/backend/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using backend.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Test> Tests { get; set; }
+    }
+}
--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,3 +1,6 @@
+using backend;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//add db
+var Configuration = builder.Configuration;
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"),
+        x => x.MigrationsHistoryTable("_EfMigrations", Configuration.GetSection("Schema").GetSection("public").Value)));
 
 var app = builder.Build();
 
